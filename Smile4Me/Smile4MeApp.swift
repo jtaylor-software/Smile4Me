8283779165c5467e04b0d71a422bc86aad2be4a9
Jeremy Taylor <jtaylor@jctaylor.org>
2025-05-20 22:08:07 -0500

    Initial Commit

---
+++ Smile4Me/Smile4MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smile4MeApp.swift
+//  Smile4Me
+//
+//  Created by Jeremy Taylor on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Smile4MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
